321543eb1a751e52598fba484d198a7f715f3c19
mabo <867379687@qq.com>
2022-09-01 02:09:40 +0800

    [cmn] 数据字典的导入

--- src/views/yygh/dict/list.vue
+++ src/views/yygh/dict/list.vue
@@ -2,9 +2,29 @@
   <div class="app-container">
     <div class="el-toolbar">
       <div class="el-toolbar-body" style="justify-content: flex-start;">
-        <el-button type="text" @click="exportData"><i class="fa fa-plus"/> 导出</el-button>
+        <el-button type="text" @click="importData"><i class="fa fa-plus" /> 导入</el-button>
+        <el-button type="text" @click="exportData"><i class="fa fa-plus" /> 导出</el-button>
       </div>
     </div>
+
+    <el-dialog title="导入" :visible.sync="dialogImportVisible" width="480px">
+      <el-form label-position="right" label-width="170px">
+        <el-form-item label="文件">
+          <el-upload
+            :multiple="false"
+            :on-success="onUploadSuccess"
+            :action="'http://localhost:8202/admin/cmn/dict/importData'"
+            class="upload-demo"
+          >
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip">只能上传xls文件，且不超过500kb</div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogImportVisible = false">取消</el-button>
+      </div>
+    </el-dialog>
 
     <el-table
       :data="list"
@@ -43,9 +63,11 @@
 
 <script>
 import dict from '@/api/yygh/dict'
+
 export default {
   data() {
     return {
+      dialogImportVisible: false,
       list: [] // 数据字典列表数组
     }
   },
@@ -67,6 +89,14 @@ export default {
     },
     exportData() {
       window.open('http://localhost:8202/admin/cmn/dict/exportData')
+    },
+    importData() {
+      this.dialogImportVisible = true
+    },
+    onUploadSuccess(response, file) {
+      this.$message.success('上传成功')
+      this.dialogImportVisible = false
+      this.getDictList(1)
     }
   }
 }
